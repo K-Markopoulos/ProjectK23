7dd1c8b7e4472f39b4e28d55ab46deb515926b1c
Dimitrios Psitos <sdi1500183@di.uoa.gr>
2018-10-22 22:01:49 +0300

    up-to-now

--- src/header.cpp
+++ src/header.cpp
@@ -3,7 +3,15 @@
 #include <math.h>
 #include "header.h"
 
-h1(int value, int n){
+int  h1(int value, int n){
   lastbits = pow(2,n) - 1
   return value & lastbits;
+}
+
+int ** histogram(tuple *R){
+  int size = sizeof(R)/sizeof(*R);
+  int * values;
+  for(int i = 0; i < size; i++){
+
+  }
 }
