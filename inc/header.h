f6c3fe87e92eab8246f167a829d3876d5e316fb9
Konstantinos Markopoulos <sdi1500091@di.uoa.gr>
2018-10-22 22:31:19 +0300

    added test relations from example

---
+++ inc/header.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdint.h>
+
+namespace db{
+
+  struct tuple {
+    int32_t key;
+    int32_t payload;
+  };
+
+  struct relation {
+    tuple *tuples;
+    uint32_t num_tuples;
+  };
+
+  struct result {
+    tuple *tuples;
+  };
+
+}
