5d3b3d6f2d8fbb62e311937272b86808ca46164a
Lazaros Zervos <lazteo2010@hotmail.com>
2018-11-01 22:37:58 +0200

    Indexing: DONE!

--- src/radix.cpp
+++ src/radix.cpp
@@ -4,7 +4,8 @@
 #include "../inc/header.h"
 
 // 1st hash function
-#define H1_LAST_BITS 8
+#define H1_LAST_BITS 3
+#define PRIME_NUM 101 
 #define h1(X) (X & ((1 << H1_LAST_BITS) - 1))
 
 
@@ -20,6 +21,20 @@ void printRelation(relation * rel, char const * name){
   std::cout << "* /Printing Relation " << name << std::endl << std::endl;
 }
 
+/*print an array for debugging,needs some fixing
+void printArray(int32_t *arr,int32_t length) {
+  for(int32_t i=0; i<length; i++) {
+    std::cout<<i<<":";
+    std::cout<<arr[i];
+    
+    std::cout<<std::endl;
+  }
+}
+*/
+
+inline int32_t h2(int32_t num) {
+  return num % PRIME_NUM; //Later we are going to find the next prime from length
+}
 
 array_int createHistogram(relation * rel){
   array_int hist;
@@ -80,6 +95,44 @@ hash_table * reorderRelation(relation * rel){
   return result;
 }
 
+void indexingAndCompareBuckets(hash_table *small,hash_table *large,result *list) {
+  int32_t *chain,*Bucket;
+  bucket sm_b,lg_b;
+  for(int32_t i=0; i<small->psum.length; i++) {
+    if( (!small->psum.data[i] && !large->psum.data[i]) || (small->psum.data[i]!=0 && large->psum.data[i]!=0) ) {
+      sm_b.low=small->psum.data[i];
+      lg_b.low=large->psum.data[i];
+
+      if(i== small->psum.length-1) {
+        sm_b.high=small->rel->num_tuples;
+        lg_b.high=large->rel->num_tuples;
+      }
+      else {
+        sm_b.high=small->psum.data[i+1];
+        lg_b.high=large->psum.data[i+1];
+      }
+      chain=new int32_t[sm_b.high];
+      Bucket=new int32_t[PRIME_NUM];          //This will be changed with the next prime number
+
+      for(int32_t j=0; j<PRIME_NUM; j++) {
+        Bucket[j]=-1;
+      }
+
+      for(int32_t l=sm_b.low; l<sm_b.high; l++) {
+        chain[l]=Bucket[h2(small->rel->tuples[l].payload)];
+        Bucket[h2(small->rel->tuples[l].payload)]=l;
+      }
+
+      /*** Until here we have the indexing part of the algorithm where we construct the chain and Bucket structures ***/
+
+      //HERE IS THE COMPARING PART//
+      
+      delete[] chain;
+      delete[] Bucket;
+    }
+  }
+}
+
 
 result * RadixHashJoin(relation * rel_R, relation * rel_S){
 
@@ -92,40 +145,13 @@ result * RadixHashJoin(relation * rel_R, relation * rel_S){
 
   printRelation(hash_table_R->rel, "R\'");
   printRelation(hash_table_S->rel, "S\'");
-  //init result
+  
+  //Here should be the initialization of the 'list'//
+  result *list;
+  if (rel_R->num_tuples < rel_S->num_tuples)
+    indexingAndCompareBuckets(hash_table_R,hash_table_S,list);
+  else
+    indexingAndCompareBuckets(hash_table_S,hash_table_R,list);
 
-  bucket * bucket_R = (bucket*) malloc(sizeof(bucket));
-  bucket * bucket_S = (bucket*) malloc(sizeof(bucket));
-  bucket * bucket_raw, * bucket_hashed;
-
-  for( int i = 0; i < hash_table_R->psum.length; i++ ){
-    bucket_R->low = hash_table_R->psum.data[i];
-    bucket_R->high = (i == hash_table_R->psum.length - 1) ?
-                      hash_table_R->rel->num_tuples - 1 :
-                      hash_table_R->psum.data[i+1];
-    bucket_S->low = hash_table_S->psum.data[i];
-    bucket_S->high = (i == hash_table_S->psum.length - 1) ?
-                      hash_table_S->rel->num_tuples - 1 :
-                      hash_table_S->psum.data[i+1];
-
-    // I might need it for debugging later
-    // std::cout << i << "\n\t"
-    //   << "bucket1 " << bucket1_low << "-" << bucket1_high << "\n\t"
-    //   << "bucket2 " << bucket2_low << "-" << bucket2_high << std::endl;
-
-    // choose smallest bucket to hash it
-    if (bucket_R->high - bucket_R->low < bucket_S->high - bucket_S->low){
-      bucket_hashed = bucket_R;
-      bucket_raw = bucket_S;
-    } else {
-      bucket_hashed = bucket_S;
-      bucket_raw = bucket_R;
-    }
-
-    // make bucket-chain hash for min
-    // result "+=" compareBuckets(raw_bucket, hashed_bucket, ... bucket-chain if needed )
-    // go to next bucket ??
-  }
-
-  // return result
+  return list;
 }
